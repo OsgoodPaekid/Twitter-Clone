0c047263f4a50f09a18084401fd1cb9d91391e3b
Osgood Ackom Amissah <95530357+OsgoodPaekid@users.noreply.github.com>
2023-09-16 22:48:22 +0100

    feat: load 20 tweets per page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
+"use client";
+import { useState } from "react";
 import { FeedItem } from "@/components/feed";
 import { CreateTweet } from "@/components/createTweet";
 
-const getPostsData = async (page, limit) => {
-  const res = await fetch(
-    `http://localhost:3333/tweets?_page=${page}&_limit=${limit}`
-  );
+const getPostsData = async () => {
+  const res = await fetch(`http://localhost:3333/tweets`);
   return res.json();
 };
 
@@ -14,7 +14,7 @@ const getUsersData = async (userId) => {
 };
 
 export default async function Home() {
-  const tweets = await getPostsData(1, 20);
+  const tweets = await getPostsData();
 
   const tweetWithUser = [];
 
@@ -29,13 +29,47 @@ export default async function Home() {
     tweetWithUser.push(tweet);
   }
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPosts = tweetWithUser.length;
+  const pageSize = 20;
+  const pages = Math.floor(totalPosts / pageSize);
+
+  const goToPrev = () => {
+    const prevPage = Math.max(currentPage - 1, 1);
+    setCurrentPage(prevPage);
+  };
+
+  const goToNext = () => {
+    const nextPage = Math.min(currentPage + 1, pages);
+    setCurrentPage(nextPage);
+  };
+
+  const start = pageSize * (currentPage - 1);
+  const end = pageSize * currentPage;
+  const postsPerPage = tweetWithUser.slice(start, end);
+
+  const canGoPrev = currentPage > 1;
+  const canGoNext = currentPage < pages;
+
   return (
     <div>
       <div className="flex flex-col space-y-6 px-2 max-h-screen overflow-scroll">
         <CreateTweet />
-        {tweetWithUser.map((post) => {
+        {postsPerPage.map((post) => {
           return <FeedItem key={post.id} {...post} />;
         })}
+      </div>
+      <div className="flex">
+        <button disabled={!canGoPrev} onClick={goToPrev}>
+          prev
+        </button>
+        <p>
+          {currentPage} of {pages}
+        </p>
+        <button disabled={!canGoNext} onClick={goToNext}>
+          next
+        </button>
       </div>
     </div>
   );

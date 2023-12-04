fcf9bf53d40f6e3b1e37bff4f7c5256cd5ba8af1
OsgoodPaekid <osgoodackom@gmail.com>
2023-12-04 22:55:46 +0000

    fix: move whoToFollow to the components directory

--- src/components/follow/index.js
+++ src/components/follow/index.js
@@ -1,18 +1,18 @@
-import { Follow } from "@/components/whoToFollow";
+import { WhoToFollow } from "@/components/whoToFollow";
 
 const getUsersData = async (limit) => {
   const res = await fetch(`http://localhost:3333/users?_limit=${limit}`);
   return res.json();
 };
 
-export async function WhoToFollow() {
+export async function WhoToFollowWidget() {
   const usersData = await getUsersData(3);
 
   return (
     <div className="w-80 h-64 bg-gray-100 rounded-md mt-4 ml-4">
       <h1 className="font-bold ml-4 mb-4">Who to follow</h1>
       {usersData.map((user) => (
-        <Follow
+        <WhoToFollow
           key={user.id}
           name={user.name}
           screen_name={user.screen_name}
